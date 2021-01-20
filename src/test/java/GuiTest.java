3749d73af4a81f7d8904d0020597d7821889a4e9
KikkiZ <2711423641@qq.com>
2021-01-20 16:55:17 +0800

    Test

--- src/test/java/GuiTest.java
+++ src/test/java/GuiTest.java
@@ -1,10 +1,5 @@
-import ink.kikkiz.gui.Main;
-import ink.kikkiz.gui.factory.ButtonFactory;
 import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Test;
 
 
 public class GuiTest extends Application {
@@ -15,7 +10,7 @@ public class GuiTest extends Application {
 
     @Override
     public void start(Stage stage) {
-        new Main().addQuestionMethod();
+        //new Main().addQuestionMethod();
 
         stage.show();
     }
